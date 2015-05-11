34be06d45f671bfb998c39c96ce5e9c36e547810
Michael Silveira <gravitypersists@gmail.com>
2015-05-11 12:51:16 -0700

    add chug and child install processes for submodules

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var watchify = require('gulp-watchify');
 var babelify = require('babelify');
+var chug = require('gulp-chug');
+var install = require("gulp-install");
 
 gulp.task('browserify', watchify(function (watchify) {
   return gulp.src(['src/leafbuilder.js'])
@@ -20,8 +22,22 @@ gulp.task('babel', function () {
     .pipe(gulp.dest('build'));
 });
 
+gulp.task('chug-children', function () {
+    // Find and run all gulpfiles under all subdirectories 
+    return gulp.src('./submodules/**/gulpfile.js')
+        .pipe(chug())
+});
+
+gulp.task('install', function() {
+  gulp.src(['./package.json', 
+            './submodules/leaf/package.json', 
+            './submodules/formsmith/package.json',])
+    .pipe(install());
+});
+
 gulp.task('watch', function () {
   gulp.watch('src/**/*.js', ['babel', 'browserify']);
 });
 
-gulp.task('default', ['babel', 'browserify', 'watch']);
+
+gulp.task('default', ['install', 'chug-children', 'babel', 'browserify', 'watch']);
--- src/leafbuilder.js
+++ src/leafbuilder.js
@@ -1,3 +1,7 @@
+const Formsmith = require('../submodules/formsmith/dist/main.js');
+const Leaf = require('../submodules/leaf/dist/main.js');
+
+
 let x = 'hello';
 
 console.log('hello');
