80b9349faed032bc70b7df5d1770709ce8560ba9
Michael Silveira <gravitypersists@gmail.com>
2015-05-12 16:59:59 -0700

    add placeholders for config buttons

--- src/layer-editor.js
+++ src/layer-editor.js
@@ -12,13 +12,35 @@ class LayerEditor extends mixin(class Base{}, events) {
       <style>
         .leafbuilder-container {
           display: inline-block;
+          position: relative;
         }
         .leafbuilder-container.hovered {
           border: 1px solid gray;
         }
+        .layer-menu {
+          position: absolute;
+          left: calc(100% + 4px);
+          top: 0;
+          margin-left: 4px;
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+        }
+        .layer-menu li {
+          width: 20px;
+          height: 20px;
+          background-color: red;
+        }
+
       </style>
+      <ul class='layer-menu'>
+        <li class='config'></li>
+      </ul>
     `);
     $parent.hover(this.onHoverIn.bind(this), this.onHoverOut.bind(this));
+    $parent.find('.config').on('click', (e) => {
+      console.log('click');
+    });
   }
 
   onHoverIn(e) {
