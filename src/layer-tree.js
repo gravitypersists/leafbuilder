3e2fa8e8cf4d8ee71d54998771a5269e42ece8fb
Michael Silveira <gravitypersists@gmail.com>
2015-05-11 22:43:56 -0700

    introduce layer tree view collection thing

---
+++ src/layer-tree.js
@@ -0,0 +1,15 @@
+
+// A sort of "view-model" but "view-collection" having a tree structure
+class LayerTree {
+
+  constructor() {
+
+  }
+
+  // nodeString looks like "3.0.1.3"
+  addLayer(nodeString, $el) {
+  }
+
+}
+
+module.exports = LayerTree;
--- src/leafbuilder.js
+++ src/leafbuilder.js
@@ -1,8 +1,16 @@
 const $ = require('jquery');
 const _ = require('lodash');
 const Leaf = require('../submodules/leaf/src/leaf');
+const LayerTree = require('./layer-tree');
 
 let configuration = require('../submodules/leaf/examples/basic.json');
 let options = { el: $('#top-node')[0] };
 let leaf = new Leaf(configuration, options);
+let tree = new LayerTree(leaf);
 
+
+_.each($('html /deep/ .leaf-layer'), (el, i) => {
+  var node = el.getAttribute('data-leaf-node');
+  let $el = $(el);
+  tree.addLayer(node.split(':')[1], $el);
+});
