5421f04b7c45d376e78fd2d5fbc9937f91bf66b6
Michael Silveira <gravitypersists@gmail.com>
2015-05-11 22:43:11 -0700

    load a leaf example from the leaf submodule

--- src/leafbuilder.js
+++ src/leafbuilder.js
@@ -1,7 +1,8 @@
-const Formsmith = require('../submodules/formsmith/src/formsmith.js');
-const Leaf = require('../submodules/leaf/src/leaf.js');
+const $ = require('jquery');
+const _ = require('lodash');
+const Leaf = require('../submodules/leaf/src/leaf');
 
+let configuration = require('../submodules/leaf/examples/basic.json');
+let options = { el: $('#top-node')[0] };
+let leaf = new Leaf(configuration, options);
 
-let x = 'hello';
-
-console.log('hello');
