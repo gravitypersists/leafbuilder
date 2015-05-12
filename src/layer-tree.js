e7db13a85305a8f43a9665b3e1385ed443b06ed3
Michael Silveira <gravitypersists@gmail.com>
2015-05-11 23:31:35 -0700

    adds internal tree data structure for layer tree

--- src/layer-tree.js
+++ src/layer-tree.js
@@ -1,15 +1,22 @@
+const _ = require('lodash');
 const LayerEditor = require('./layer-editor.js');
 
 // A sort of "view-model" but "view-collection" having a tree structure
 class LayerTree {
 
   constructor() {
-
+    this.data = {};
   }
 
   // nodeString looks like "3.0.1.3"
   addLayer(nodeString, $el) {
+    let layerNode = _.reduce(nodeString.split('.'), (res, n) => {
+      res[n] = res[n] || {};
+      return res[n];
+    }, this.data);
     let le = new LayerEditor($el);
+    layerNode.$el = $el;
+    layerNode.editor = le;
   }
 
 }
