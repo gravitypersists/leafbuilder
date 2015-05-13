f789399adf93fd9cb73f2eb119f62ca577229eb0
Michael Silveira <gravitypersists@gmail.com>
2015-05-13 14:29:18 -0700

    improvements layer tree data structure, adds reference to parents

--- src/layer-tree.js
+++ src/layer-tree.js
@@ -5,20 +5,24 @@ const LayerEditor = require('./layer-editor.js');
 class LayerTree {
 
   constructor() {
-    this.data = {};
+    this.data = null;
+    this.shiftOn = false;
   }
 
   // nodeString looks like "3.0.1.3"
   addLayer(nodeString, $el) {
     let editor = new LayerEditor($el);
 
-    let parentNode = null;
     let split = nodeString.split('.');
     if (split.length == 1) {
-      parentNode = this.data[split[0]] = {};
+      // manually construct top level data structure
+      this.data = { children: [] };
+      this.data.children[nodeString] = { $el, editor, parentNode:this.data, children: [] };
     } else {
-      parentNode = _.reduce(_.initial(split), (res, n) => res[n], this.data);
-      parentNode[_.last(split)] = { $el, editor }
+      // traverse the tree to get the parent node, then add it to the structure
+      let parentNode = _.reduce(_.initial(split), (res, n) => res.children[n], this.data);
+      // worth noting that this data structure is self-referential, so cannot be stringified 
+      parentNode.children[_.last(split)] = { $el, editor, parentNode, children: [] };
     }
 
     editor.on('hovered', () => {
