3de8a60f77d981a7dabec293cd7c5bb6d279f64d
Michael Silveira <gravitypersists@gmail.com>
2015-05-18 16:10:26 -0700

    improve gulp task to actually utilize watchify, bringing build times from 8s down to ms

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,34 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var babel = require('gulp-babel');
-var watchify = require('gulp-watchify');
+var watchify = require('watchify');
 var babelify = require('babelify');
 var chug = require('gulp-chug');
-var install = require("gulp-install");
+var install = require('gulp-install');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 
-gulp.task('browserify', watchify(function (watchify) {
-  return gulp.src(['src/leafbuilder.js'])
-    .pipe(watchify({
-      watch: false,
-      setup: function (bundle) {
-        bundle.transform(babelify);
-      }
-    }))
-    .pipe(gulp.dest('build/'));
-}));
+var bundler = watchify(browserify('./src/leafbuilder.js', watchify.args));
+bundler.transform('babelify');
+gulp.task('browserify', bundle);
+bundler.on('update', bundle);
+
+function bundle() {
+  var start = new Date().getTime();
+
+  var b = bundler.bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('./build'));
+
+  var end = new Date().getTime();
+  var time = end - start;
+
+  gutil.log('[browserify]', 'rebundle took ', gutil.colors.cyan(time + ' ms'));
+  return b;
+}
 
 gulp.task('babel', function () {
   return gulp.src('src/**/*.js')
@@ -22,23 +36,19 @@ gulp.task('babel', function () {
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('chug-children', function () {
-    // Find and run all gulpfiles under all subdirectories 
-    return gulp.src('./submodules/**/gulpfile.js')
-        .pipe(chug())
-});
+// gulp.task('chug-children', function () {
+//     // Find and run all gulpfiles under all subdirectories 
+//     return gulp.src('./submodules/**/gulpfile.js')
+//         .pipe(chug())
+// });
 
-gulp.task('install', function() {
-  gulp.src(['./package.json', 
-            './submodules/leaf/package.json', 
-            './submodules/formsmith/package.json',])
-    .pipe(install());
-});
-
-gulp.task('watch', function () {
-  gulp.watch(['src/**/*.js', 'submodules/leaf/src/**'], ['babel', 'browserify']);
-});
+// gulp.task('install', function() {
+//   gulp.src(['./package.json', 
+//             './submodules/leaf/package.json', 
+//             './submodules/formsmith/package.json',])
+//     .pipe(install());
+// });
 
 
 // gulp.task('default', ['install', 'chug-children', 'babel', 'browserify', 'watch']);
-gulp.task('default', ['babel', 'browserify', 'watch']);
+\gulp.task('default', ['browserify']);
