a692bb077c6efe12f696c336489d307ae07ed721
Michael Silveira <gravitypersists@gmail.com>
2015-05-26 10:02:26 -0700

    adds logical statement and updates leaf submodule

--- src/leafbuilder.js
+++ src/leafbuilder.js
@@ -16,7 +16,8 @@ configuration.manifests = {
   Picker: require('../submodules/leaf/src/elements/picker/manifest.json'),
   Image: require('../submodules/leaf/src/elements/image/manifest.json'),
   Switch: require('../submodules/leaf/src/elements/switch/manifest.json'),
-  Switch: require('../submodules/leaf/src/elements/event-button/manifest.json')
+  EventButton: require('../submodules/leaf/src/elements/event-button/manifest.json'),
+  LogicalStatement: require('../submodules/leaf/src/elements/logical-statement/manifest.json'),
 }
 let config = new ConfigModel(configuration);
 
