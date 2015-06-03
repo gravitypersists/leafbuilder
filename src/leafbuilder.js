202b9353978056f5e7f9ed492056b2374169e1a8
Michael Silveira <gravitypersists@gmail.com>
2015-06-02 17:33:50 -0700

    use brfs to load base formbuilder styles

--- src/leafbuilder.js
+++ src/leafbuilder.js
@@ -4,6 +4,14 @@ const Leaf = require('../submodules/leaf/src/leaf');
 const LayerTree = require('./layer-tree');
 const Toolbox = require('./toolbox');
 const ConfigModel = require('./config');
+const fs = require('fs');
+// TODO: look into using webpack, or finding a proper way to package
+// css in with applications
+const baseStyles = fs.readFileSync(__dirname + '/../styles/leafbuilder.css', 'utf8')
+const fsStyles = fs.readFileSync(__dirname + '/../submodules/formsmith/styles/formsmith.css', 'utf8')
+const fsExtras = fs.readFileSync(__dirname + '/../styles/formsmith-custom.css', 'utf8')
+
+let styles = baseStyles + fsStyles + fsExtras;
 
 let manifests = {
   Text: require('../submodules/leaf/src/elements/text/manifest.json'),
@@ -27,17 +35,22 @@ class LeafBuilder {
   constructor(el, configuration) {
     this.$el = $(el);
     this.$el.html(`
-      <div class="leaf"></div>
-      <div class="toolbox"></div>
-      <div id="detached"></div>
+      <style>${ styles }</style>
+      <div class="leafbuilder-main-container">
+        <div class="leaf"></div>
+        <div class="toolbox"></div>
+        <div id="detached"></div>
+      </div>
     `);
+    let $main = this.$el.find('.leafbuilder-main-container');
     configuration.manifests = manifests;
-    let options = { el: this.$el.children('.leaf')[0] };
+    let options = { el: $main.children('.leaf')[0] };
     let leaf = new Leaf(configuration, options);
-    let toolbox = new Toolbox(this.$el.children('.toolbox'), leaf, manifests, config);
+    let toolbox = new Toolbox($main.children('.toolbox'), leaf, manifests, config);
     let config = new ConfigModel(configuration);
-    let tree = new LayerTree(leaf, this.$el, config, toolbox);
+    let tree = new LayerTree(leaf, $main, config, toolbox);
 
+    // TODO need to scope this
     loadElementsIntoTree($('html /deep/ .leaf-layer'), tree);
 
     // toolbox knows when a portion of the tree has been rerendered.
